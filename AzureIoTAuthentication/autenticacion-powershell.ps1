4210ff8a275c9a4e649cc4eaf0d6f001a9f344d3
Luis Garreta <lgarreta@gmail.com>
2017-01-11 16:17:49 -0500

    Added authentication script

---
+++ AzureIoTAuthentication/autenticacion-powershell.ps1
@@ -0,0 +1,15 @@
+﻿
+$login = Login-AzureRmAccount
+$tenantId = $login.Context.Tenant.TenantId
+$suscriptionId = $login.Context.Tenant.TenantId
+$nombreAplicacion = "progiothubapp"
+$clave = "iothub2017A"
+
+$app = New-AzureRmADApplication -DisplayName $nombreAplicacion -HomePage "http://$nombreAplicacion/home" -IdentifierUris "http://$nombreAplicacion" -Password $clave
+$serv = New-AzureRmADServicePrincipal -ApplicationId $app.ApplicationId
+$role = New-AzureRmRoleAssignment -RoleDefinitionName Owner -ServicePrincipalName $app.ApplicationId
+
+echo "TenantId: $tenantId"
+echo "SubscriptionId: $suscriptionId"
+echo "ApplicationId: $app.ApplicationId"
+echo "Password: $clave"
